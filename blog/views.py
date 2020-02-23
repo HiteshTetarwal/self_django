0f9cc3b3c75f6511a9c40c53b210e7a685f392ee
unknown <imhitesh007@gmail.com>
2020-02-23 20:06:23 +0530

    added a post delete view but check the userpassestextmixin

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Post
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 
 def home(request):
@@ -27,6 +27,32 @@ class PostCreateView(LoginRequiredMixin, CreateView):
 	def form_valid(self,form):
 		form.instance.author=self.request.user
 		return super().form_valid(form)
+	def test_func(self):
+		post = self.get_object
+		if self.request.user == post.author:
+			return True
+		return False
+
+class PostUpdateView(LoginRequiredMixin,UpdateView):
+	model = Post
+	fields = ['title','content']
+
+	def form_valid(self,form):
+		form.instance.author=self.request.user
+		return super().form_valid(form)
+	def test_func(self):
+		post = self.get_object
+		if self.request.user == post.author:
+			return True
+		return False
+
+class PostDeleteView(LoginRequiredMixin, DeleteView):
+	model = Post
+	def test_func(self):
+		post = self.get_object
+		if self.request.user == post.author:
+			return True
+		return False
 
 def about(request):
 

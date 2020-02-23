7bb335d78cb335eae21a580d40e0b85286cd7663
unknown <imhitesh007@gmail.com>
2020-02-23 22:50:58 +0530

    added new post option with all integrations

--- blog/views.py
+++ blog/views.py
@@ -27,13 +27,8 @@ class PostCreateView(LoginRequiredMixin, CreateView):
 	def form_valid(self,form):
 		form.instance.author=self.request.user
 		return super().form_valid(form)
-	def test_func(self):
-		post = self.get_object
-		if self.request.user == post.author:
-			return True
-		return False
 
-class PostUpdateView(LoginRequiredMixin,UpdateView):
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin,UpdateView):
 	model = Post
 	fields = ['title','content']
 
@@ -41,15 +36,16 @@ class PostUpdateView(LoginRequiredMixin,UpdateView):
 		form.instance.author=self.request.user
 		return super().form_valid(form)
 	def test_func(self):
-		post = self.get_object
+		post = self.get_object()
 		if self.request.user == post.author:
 			return True
 		return False
 
-class PostDeleteView(LoginRequiredMixin, DeleteView):
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
 	model = Post
+	success_url = '/'
 	def test_func(self):
-		post = self.get_object
+		post = self.get_object()
 		if self.request.user == post.author:
 			return True
 		return False

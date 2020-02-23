9f092c98343b547c46a19dfbfda52bf14a7f8962
unknown <imhitesh007@gmail.com>
2020-02-23 14:04:24 +0530

    added new blog post fields template

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Post
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 
 
 def home(request):
@@ -18,6 +19,14 @@ class PostListView(ListView):
 
 class PostDetailView(DetailView):
 	model = Post
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+	model = Post
+	fields = ['title','content']
+
+	def form_valid(self,form):
+		form.instance.author=self.request.user
+		return super().form_valid(form)
 
 def about(request):
 
